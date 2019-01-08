81aab237bbe240f1b9000e7b683b855d67681f6f
Melvin <melvinkusters0@gmail.com>
2019-01-08 11:24:05 +0100

    Spring Initializr scaffolding.

---
+++ src/main/java/com/mellykusjes/chessmaxws/ChessmaxWsApplication.java
@@ -0,0 +1,14 @@
+package com.mellykusjes.chessmaxws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessmaxWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessmaxWsApplication.class, args);
+	}
+
+}
+
